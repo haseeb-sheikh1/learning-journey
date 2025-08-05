398151512babcb464514ee6611b8f555123dfc5e
SHEIKH HASEEB-UR-REHMAN <154455549+haseeb-sheikh1@users.noreply.github.com>
2025-08-05 18:08:12 +0500

    Add files via upload

---
+++ operatorOverloading.cpp
@@ -0,0 +1,37 @@
+	#include<iostream>
+	using namespace std;
+	class Distance{
+		private:
+			double feet;
+			double inches;
+		
+		public:
+		
+			Distance(double feet, double inches): feet(feet), inches(inches){
+				
+			}
+			Distance (){
+				
+			}
+			
+		bool operator== (const Distance &other){
+		if ((this->feet == other.feet) && (this->inches == other.inches)){
+		 return true;
+		}
+		else {
+			return false;
+		}
+	}
+		 
+
+	};
+	 int main ()
+	 {
+	 	
+	 	Distance d1(10,20);
+	 	Distance d2(30,33);
+	 	bool isEqual;
+		isEqual = d1==d2;
+	 
+	 	 return 0;
+	 }
